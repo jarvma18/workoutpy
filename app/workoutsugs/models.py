352841f34dd85755e041b8fd2b049882281532f3
jarvma18 <markusmjarvinen@outlook.com>
2024-06-02 14:47:17 +0300

    Casting suggestion to str and gave was_created_recently function a return type.

--- app/workoutsugs/models.py
+++ app/workoutsugs/models.py
@@ -7,6 +7,6 @@ class Suggestions(models.Model):
   suggestion = models.CharField(max_length=750)
   created_at = models.DateTimeField('Date created')
   def __str__(self) -> str:
-    return self.suggestion
-  def was_created_recently(self):
+    return str(self.suggestion)
+  def was_created_recently(self) -> bool:
     return self.created_at >= timezone.now() - datetime.timedelta(days=1)
