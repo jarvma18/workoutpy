93b20b9a69c35ba960b2b6f5414b81edb59c15fb
jarvma18 <markusmjarvinen@outlook.com>
2024-06-02 13:33:04 +0300

    Some naming and style fixes.

--- app/workoutsugs/models.py
+++ app/workoutsugs/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class Suggestions(models.Model):
+  suggestion = models.CharField(max_length=750)
+  created_at = models.DateTimeField('Date created')
+  def __str__(self) -> str:
+    return self.suggestion
+  def was_created_recently(self):
+    return self.created_at >= timezone.now() - datetime.timedelta(days=1)
